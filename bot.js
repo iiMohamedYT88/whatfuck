8a601212ddea49f996871cbf3b8029721e4efb05
iiMohamedYT88 <46201203+iiMohamedYT88@users.noreply.github.com>
2019-01-08 08:59:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "m7";
+const prefix = "3";
 /////////////////////////
 ////////////////////////
 
